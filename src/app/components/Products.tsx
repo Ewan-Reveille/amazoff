3ffec62724e9ca0f65d2fe2f1bf91eab5dc0fa38
Ewan-Reveille <ewan.reveille23@gmail.com>
2024-10-08 21:29:23 +0200

    improved responsivness

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -2,35 +2,17 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 export default function Products() {
+  const getRandomImageUrl = () => `https://picsum.photos/200?random=${Math.floor(Math.random() * 1000)}`;
   return (
     <div className="md:mx-12 md:px-28 my-24">
       <h2 className="text-3xl font-bold mb-8">Nos produits</h2>
       
       <div className="flex flex-wrap justify-center sm:justify-start gap-6">
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
-        <div className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
-          <ProductCard size="small" />
-        </div>
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index} className="flex-[1_1_100%] sm:flex-[1_1_calc(50%-1rem)] lg:flex-[1_1_calc(33%-1rem)] xl:flex-[1_1_calc(25%-1rem)] flex justify-center xl:block max-w-[350px]">
+            <ProductCard size="small" image={getRandomImageUrl()} />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/app/components/Soldes.tsx
+++ src/app/components/Soldes.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 export default function Soldes() {
+  const getRandomImageUrl = () => `https://picsum.photos/200?random=${Math.floor(Math.random() * 1000)}`;
+
   return (
     <article className="w-full md:w-[82%] md:mx-[9%] pb-12">
       <h2 className="text-4xl font-bold mb-12">Nos soldes</h2>
 
       <div className="relative">
-
         <div className="bg-white rounded-2xl md:p-10 shadow-2xl transition-shadow hover:shadow-3xl duration-300 ease-in-out md:px-[120px]">
           <div className='w-full flex flex-wrap justify-between gap-4'>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {Array.from({ length: 3 }).map((_, index) => (
+              <ProductCard key={index} image={getRandomImageUrl()} />
+            ))}
           </div>
         </div>
       </div>
